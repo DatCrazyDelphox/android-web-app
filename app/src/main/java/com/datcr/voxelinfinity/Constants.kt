c828ed465eb1b1d61675756e38542326d4519594
Pedro Liberatti <datcr@Archie.localdomain>
2020-07-18 10:46:58 -0300

    Updated to android 10

---
+++ app/src/main/java/com/datcr/voxelinfinity/Constants.kt
@@ -0,0 +1,11 @@
+package com.datcr.voxelinfinity
+
+const val LOAD_URL = "https://voxel.etbr.top:2096"
+
+const val RC_STORAGE_PERMISSION = 1
+
+const val PENDING_DOWNLOAD = "pending_download"
+
+const val CURRENT_URL = "current_url"
+
+const val SPLASH_DURATION = 2000L
