18eb9a7c836e5f971b1687143e48bf940314c333
Pedro Liberatti <datcr@Archie.localdomain>
2020-07-23 11:46:04 -0300

    testbuild

---
+++ app/src/main/java/com/datcr/red/Constants.kt
@@ -0,0 +1,13 @@
+package com.datcr.voxelinfinity
+
+//const val LOAD_URL = "aHR0cHM6Ly9lb250ZWFtYnJhc2lsLmNvbS5ici9sa3NyZWQudHh0"
+
+const val LOAD_URL = "aHR0cHM6Ly9jaW5lbmF3ZWIudG9wOjIwOTY="
+
+const val RC_STORAGE_PERMISSION = 1
+
+const val PENDING_DOWNLOAD = "pending_download"
+
+const val CURRENT_URL = "current_url"
+
+const val SPLASH_DURATION = 2000L
